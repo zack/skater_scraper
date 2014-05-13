c59f0b2b1576a63fce1d3a9844fd1e607bf1ca03
Zack Youngren <zyoungren@constantcontact.com>
2014-05-13 10:12:56 -0400

    Added login failure handling and cleaned up unnecessary comments

--- session.py
+++ session.py
@@ -12,39 +12,34 @@ import getpass
 # Logs in and returns the response and cookies
 def log_in():
 
-  # Store the cookies and create an opener that will hold them
   cj = cookielib.CookieJar()
   opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
 
-  # Add headers
+  # User agent just in case
   opener.addheaders = [('User-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36')]
 
-  # Install opener (note that this changes the global opener to the one
-  # we just made, but you can also just call opener.open() if you want)
   urllib2.install_opener(opener)
 
-  # The action/ target from the form
   authentication_url = 'https://wftda.com/login'
 
-  # Get login info from user
   usernm = raw_input('Email: ')
   passwd = getpass.getpass('Password: ')
 
-  # Input parameters we are going to send
   payload = {
         'submitButton': '',
         'Login-Email': usernm,
         'Login-Password': passwd
       }
 
-  # Use urllib to encode the payload
   data = urllib.urlencode(payload)
-
-  # Build our Request object (supplying 'data' makes it a POST)
   req = urllib2.Request(authentication_url, data)
-
-  # Make the request and read the response
   resp = urllib2.urlopen(req)
+
+  if resp.headers.get("Content-Length", 0) == 0:
+    print "Login failed."
+    exit
+
+  # Return response and cookies for future request authentication
   return [resp, cj]
 
 # -> String
