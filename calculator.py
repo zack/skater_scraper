24069abf397cbf3e8115ce053a39cba6ddef3951
Zack Youngren <zyoungren@constantcontact.com>
2014-06-25 12:11:58 -0400

    and now it doesnt completely suck

--- calculator.py
+++ calculator.py
@@ -23,7 +23,9 @@ member_f = 0
 apprentice_f = 0
 
 counts = {
+        'Total': {},
         'Members': {
+            'Total': 0,
             'Numeric': 0,
             'Alphanumeric': 0,
             'Long': 0,
@@ -31,6 +33,7 @@ counts = {
             'Terrible' : 0
             },
         'Apprentices': {
+            'Total': 0,
             'Numeric': 0,
             'Alphanumeric': 0,
             'Long': 0,
@@ -43,28 +46,27 @@ counts = {
 # Let's look a skater
 for skater in data:
     num = skater['skater_number']
-    if skater['league_status'] == "Apprentice":
-        if is_number(num):
-            counts['Apprentices']['Numeric'] += 1
-        else:
-            apprentice_a += 1
-            counts['Apprentices']['Alphanumeric'] += 1
-            if len(num) == 4:
-                counts['Apprentices']['Terrible'] += 1
-        if len(num) > 2:
-            counts['Apprentices']['Long'] += 1
-        else:
-            counts['Apprentices']['Short'] += 1
+
+    if skater['league_status'] == 'Member':
+        skater_type = 'Members'
     else:
-        if is_number(num):
-            counts['Members']['Numeric'] += 1
-        else:
-            counts['Members']['Alphanumeric'] += 1
-            if len(num) == 4:
-                counts['Members']['Terrible'] += 1
-        if len(num) > 2:
-            counts['Members']['Long'] += 1
-        else:
-            counts['Members']['Short'] += 1
+        skater_type = 'Apprentices'
+
+    counts[skater_type]['Total'] += 1
+
+    if is_number(num):
+        counts[skater_type]['Numeric'] += 1
+    else:
+        counts[skater_type]['Alphanumeric'] += 1
+        if len(num) == 4:
+            counts[skater_type]['Terrible'] += 1
+    if len(num) > 2:
+        counts[skater_type]['Long'] += 1
+    else:
+        counts[skater_type]['Short'] += 1
+
+for field in counts['Members']:
+    counts['Total'][field] = (counts['Members'][field] +
+                              counts['Apprentices'][field])
 
 print json.dumps(counts, indent=4)
